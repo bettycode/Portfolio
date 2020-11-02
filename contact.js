36b979f14595f9e92c032c1721919236fc313adf
bettycode <betty.haile.us@gmail.com>
2020-11-01 21:49:25 -0500

    added js file and modified some files

---
+++ contact.js
@@ -0,0 +1,23 @@
+    var form = $("#form");
+    var button = $("#submitC");
+    var first = $("#first").val();
+    var last = $("#last").val();
+    var email = $("#email").val();
+    var message = $("#text").val();
+
+
+    
+   
+    
+    
+
+
+
+
+ 
+
+
+
+
+
+
